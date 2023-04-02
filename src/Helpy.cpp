0fb808340da8b917abb578faf485ab820fa2c8e3
shamiko <up202108672@up.pt>
2023-04-02 19:00:32 +0100

    Helpy is trying to exist

--- src/Helpy.cpp
+++ src/Helpy.cpp
@@ -166,312 +166,6 @@ int Helpy::readStation(){
     return station;
 }
 
-/**
- * @brief reads a location based on an airport OR a city OR a country OR coordinates
- * @complexity O(n^2)
- * @return the code of the airport situated in said location
- */
-string Helpy::readLocation(string instruction){
-    if (instruction.empty()){
-        ostringstream instr;
-        instr << "With which of the following would you like to define the desired " << BOLD << "location"
-              << RESET << "?" << endl << endl
-              << "* Airport" << endl
-              << "* City" << endl
-              << "* Country" << endl
-              << "* Coordinates";
-
-        instruction = instr.str();
-    }
-
-    uSet<string> options = {"airport", "city", "country", "coordinates"};
-    string choice = readInput(instruction, options);
-
-    return readAirport();
-}
-
-/**
- * @brief reads from the console the selection of cities to consider
- * @complexity O(n^2)
- * @return the codes of the airports that are in the considered cities
- */
-uSet<string> Helpy::readUsableCities(){
-    uSet<string> airports;
-
-    cout << endl << YELLOW << BREAK << RESET << endl << endl;
-    cout << "Please type the " << BOLD << "names" << RESET << " of the " << YELLOW << "cities" << RESET << " you would like to " << GREEN << "use"
-         << RESET << ", separated by a comma (ex: Porto,Lisbon,...).\n"
-         << "If there is no city you would particularly like to visit, press Enter.\n\n";
-
-    // cities to USE
-    string line; getline(cin, line);
-    lowercase(line, true); line += ",";
-
-    istringstream line_(line);
-
-    for (string temp; getline(line_, temp, ',');){
-        properName(temp);
-
-        if (cityNames.find(temp) == cityNames.end()){
-            continue;
-        }
-
-        for (const string& s : cityNames[temp]){
-            airports.insert(airportNames[s]);
-        }
-    }
-
-    if (!airports.empty()) return airports;
-
-    // cities to AVOID
-    airports = airportCodes;
-
-    cout << endl << YELLOW << BREAK << RESET << endl << endl;
-    cout << "Please type the " << BOLD << "names" << RESET << " of the " << YELLOW << "cities" << RESET
-         << " you would like to " << RED << "avoid" << RESET << ", separated by a comma (ex: Porto,Lisbon,...).\n"
-         << "If there is no city you wish to avoid, press Enter.\n\n";
-
-    getline(cin, line);
-    lowercase(line, true); line += ",";
-
-    line_.clear(); line_.str(line);
-
-    for (string temp; getline(line_, temp, ',');){
-        properName(temp);
-
-        if (cityNames.find(temp) == cityNames.end()){
-            continue;
-        }
-
-        for (const string& s : cityNames[temp]){
-            airports.erase(airportNames[s]);
-        }
-    }
-
-    return airports;
-}
-
-
-/**
- * @brief reads the airports that the user wants to use
- * @complexity O(n^2)
- * @return the codes of the airports the user selects
- */
-uSet<string> Helpy::readUsableAirports(){
-    uSet<string> airports;
-
-    cout << endl << YELLOW << BREAK << RESET << endl << endl;
-    cout << "Please type the " << BOLD << "codes" << RESET << " of the " << YELLOW << "airports" << RESET << " you would like to "
-         << GREEN << "use" << RESET << ", separated by a comma (ex: OPO,PXO,...).\n"
-         << "If there is no airport you would particularly like to use, press Enter.\n\n";
-
-    // airports to USE
-    string line; getline(cin, line);
-    lowercase(line, true); line += ",";
-
-    istringstream line_(line);
-
-    for (string temp; getline(line_, temp, ',');){
-        if (temp.size() > 3){
-            properName(temp);
-            temp = airportNames[temp];
-        }
-        else{
-            lowercase(temp, true);
-        }
-
-        if (airportCodes.find(temp) != airlineCodes.end()){
-            airports.insert(temp);
-        }
-    }
-
-    if (!airports.empty()) return airports;
-
-    // airlines to AVOID
-    airports = airportCodes;
-
-    cout << endl << YELLOW << BREAK << RESET << endl << endl;
-    cout << "Please type the " << BOLD << "codes" << RESET << " of the " << YELLOW << "airports" << RESET << " you would like to "
-         << RED << "avoid" << RESET << ", separated by a comma (ex: OPO,PXO,...).\n"
-         << "If there is no airport you wish to avoid, simply press Enter.\n\n";
-
-    getline(cin, line);
-    lowercase(line, true); line += ",";
-
-    line_.clear(); line_.str(line);
-
-    for (string temp; getline(line_, temp, ',');){
-        if (temp.size() > 3) temp = airportNames[temp];
-
-        auto it = airports.find(temp);
-
-        if (it != airports.end()){
-            airports.erase(it);
-        }
-    }
-
-    // read cities and countries to use
-    uSet<string> cityAirports = readUsableCities();
-    uSet<string> countryAirports = readUsableCountries();
-
-    for (auto it = airports.begin(); it != airports.end();){
-        bool foundCity = (cityAirports.find(*it) != cityAirports.end());
-        bool foundCountry = (countryAirports.find(*it) != countryAirports.end());
-
-        if (foundCity && foundCountry){
-            it++;
-            continue;
-        }
-
-        it = airports.erase(it);
-    }
-
-    return airports;
-}
-
-/**
- * @brief reads the Airlines and the Airports the user would like to use
- * @complexity O(n^2)
- * @return array of two unordered_sets of strings, both of which containing codes (of Airlines and Airports, respectively)
- */
-uSet<string>* Helpy::readRestrictions(){
-    auto use = new uSet<string>[2];
-
-    ostringstream instr;
-    instr << "To better meet your requirements, I will now ask you some questions regarding which " << YELLOW << "airlines" << RESET
-          << ", " << YELLOW << "airports" << RESET << " and " << YELLOW << "countries" << RESET << " you would like to use or avoid.\n"
-          << "However, if you do not want to have such restrictions, I can skip this part.\n\n"
-          << "Would you like to answer the questions? (Yes/No)";
-
-    uSet<string> options = {"yes", "no", "y", "n"};
-    string ans = readInput(instr.str(), options);
-
-    if (ans == "no" || ans == "n"){
-        return nullptr;
-    }
-
-    cout << endl << YELLOW << BREAK << RESET << endl << endl;
-    cout << "Very well. Let us start.\n\n";
-
-    use[0] = readUsableAirlines();
-    use[1] = readUsableAirports();
-
-    return use;
-}
-
-/**
- * @brief prints in a table the name, city and country of the Airports in the argument list
- * @complexity O(n)
- * @param airports container with the Airports whose information will be printed
- * @param columnNames list with the names of each table column
- */
-template <typename T>
-void Helpy::printAirports(T airports, const list<string>& columnNames){
-    cout << endl << YELLOW << BREAK << RESET << endl;
-
-    if (airports.empty()){
-        cout << endl << "It appears there are no results that match your request..." << endl;
-        return;
-    }
-
-    cout << endl << "These are the results of my search:" << endl << endl;
-
-    // setting up the table
-    fort::char_table table;
-    table.set_border_style(FT_NICE_STYLE);
-
-    table.row(0).set_cell_content_text_style(fort::text_style::bold);
-    table.row(0).set_cell_content_fg_color(fort::color::yellow);
-    table << fort::header;
-
-    auto namesIt = columnNames.begin();
-    for (int i = 0; i < (int) columnNames.size(); i++){
-        table << *namesIt++;
-        table.column(i).set_cell_text_align(fort::text_align::center);
-    }
-
-    table << fort::endr;
-
-    // filling the table with data
-    uSet<string> citiesReached, countriesReached;
-
-    for (const auto& p : airports){
-        const Airport& a = p.first;
-
-        table << a.getName() << p.second << fort::endr;
-
-        string airportInfo = '(' + a.getCity() + ", " + a.getCountry() + ')';
-
-        citiesReached.insert(a.getCity());
-        countriesReached.insert(a.getCountry());
-
-        table << airportInfo << fort::endr;
-
-        table << fort::separator;
-    }
-
-    cout << table.to_string();
-
-    cout << endl << "In total:" << endl
-         << "* " << YELLOW << "Cities" << RESET " reached: " << citiesReached.size() << endl
-         << "* " << YELLOW << "Countries" << RESET << " reached: " << countriesReached.size() << endl;
-}
-
-/**
- * @brief prints all the flights of an Airport in a table
- * @complexity O(n * |E|)
- * @param airport the code of the Airport whose flights will be printed
- */
-void Helpy::printFlights(const string& airport){
-    // setting up the table
-    fort::char_table table;
-    table.set_border_style(FT_NICE_STYLE);
-
-    for (int i = 0; i < 2; i++)
-        table.column(i).set_cell_text_align(fort::text_align::center);
-
-    table.row(0).set_cell_content_text_style(fort::text_style::bold);
-    table.row(0).set_cell_content_fg_color(fort::color::yellow);
-    table << fort::header
-          << "Airport" << "Airlines" << fort::endr;
-
-    // filling the table with data
-    int flightNum = 0;
-    uSet<string> citiesReached, countriesReached;
-
-    for (const auto e : graph.getFlights(airport)){
-        const Airport& dest = e->dest;
-        auto it = e->airlines.begin();
-
-        table << dest.getName() << it++->getName() << fort::endr;
-
-        string destInfo = '(' + dest.getCity() + ", " + dest.getCountry() + ')';
-
-        flightNum++;
-        citiesReached.insert(dest.getCity());
-        countriesReached.insert(dest.getCountry());
-
-        if (it == e->airlines.end()){
-            table << destInfo << "" << fort::endr;
-        }
-        else{
-            table << destInfo << it++->getName() << fort::endr;
-        }
-
-        while (it != e->airlines.end()){
-            table << "" << it++->getName() << fort::endr;
-        }
-
-        table << fort::separator;
-    }
-
-    cout << table.to_string();
-
-    cout << endl << "In total:" << endl
-         << "* Number of " << YELLOW << "flights" << RESET ": " << flightNum << endl
-         << "* " << YELLOW << "Cities" << RESET " reached: " << citiesReached.size() << endl
-         << "* " << YELLOW << "Countries" << RESET << " reached: " << countriesReached.size() << endl;
-}
 
 /**
  * @brief allows the user to choose the mode of the UI
@@ -666,15 +360,11 @@ bool Helpy::process_command(string& s1, string& s2, string& s3){
  * @param finish station where we end up
  */
 void Helpy::calculateMaximumTrains(){
-    string start = readAirport();
-    string finish = readAirport();
-    const Airport& a = graph.getAirport(airport);
+    int start = readStation();
+    int finish = readStation();
 
     cout << endl << YELLOW << BREAK << RESET << endl;
-    cout << endl << "The airport " << a.getName() << " (" << a.getCity() << ", " << a.getCountry()
-         << ") has the following flights:" << endl << endl;
 
-    printFlights(airport);
 }
 
 
@@ -683,14 +373,11 @@ void Helpy::calculateMaximumTrains(){
  * @complexity O(n * |E|)
  */
 void Helpy::determineMostTrains(){
-    string airport = readAirport();
-    const Airport& a = graph.getAirport(airport);
+    int airport = readStation();
+
 
     cout << endl << YELLOW << BREAK << RESET << endl;
-    cout << endl << "The airport " << a.getName() << " (" << a.getCity() << ", " << a.getCountry()
-         << ") has the following flights:" << endl << endl;
 
-    printFlights(airport);
 }
 
 /**
@@ -699,12 +386,7 @@ void Helpy::determineMostTrains(){
  * @param number number of districts (or municipalities) to show
  */
 void Helpy::determineBudgetNeed(){
-    string number = readAirport();
-    const Airport& a = graph.getAirport(airport);
+    int number = readStation();
 
     cout << endl << YELLOW << BREAK << RESET << endl;
-    cout << endl << "The airport " << a.getName() << " (" << a.getCity() << ", " << a.getCountry()
-         << ") has the following flights:" << endl << endl;
-
-    printFlights(airport);
 }
